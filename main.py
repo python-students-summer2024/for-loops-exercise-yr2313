e87db9ec0b78c4be8dbe8afad2a0fbcb66145915
bloombar <amos@cs.nyu.edu>
2023-11-01 04:13:21 -0400

    pylinted

--- main.py
+++ main.py
@@ -7,29 +7,36 @@ Feel free to comment/uncomment or otherwise modify any line of code in this file
 #### VIRUS TRANSMISSION ####
 # calculate how many people will be infected by a population of 10 infected people over 14 days, assuming a transmission rate of 1.2
 from virus_transmission import calculate_infections
+
 starting_infections = 2
 reproduction_rate = 1.1
 num_days = 62
-total_infections = calculate_infections(starting_infections, reproduction_rate, num_days) # calculate total infections
-print("{} people will go on to create a total of {} infections over {} days, assuming a reproduction rate of {}".format(starting_infections, total_infections, num_days, reproduction_rate) )
+total_infections = calculate_infections(
+    starting_infections, reproduction_rate, num_days
+)  # calculate total infections
+print(
+    "{} people will go on to create a total of {} infections over {} days, assuming a reproduction rate of {}".format(
+        starting_infections, total_infections, num_days, reproduction_rate
+    )
+)
 
 
 # #### NUMBER GUESSING ####
 # ask the user to guess a number between 1 and 10... give them 5 attempts
 from number_guessing import guess_number
+
 if guess_number(1, 10, 5):
-  print("You guessed correctly!") # they guessed correctly!
+    print("You guessed correctly!")  # they guessed correctly!
 else:
-  print("Sorry, all your guesses were incorrect!") # they guessed incorrectly!
+    print("Sorry, all your guesses were incorrect!")  # they guessed incorrectly!
 
 
 # #### FOR LOOPING TURTLES ####
 # draw four overlapping squares, and then a five-pointed star
 from loopy_turtles import create_turtle, draw_square, draw_star
-t = create_turtle('red', 'yellow') # create a turtle object
-for x in range(-200, 0, 25): # loop four times
-  draw_square(t, x, x, 100, 'left', '#F5DEB3') # draw a square
 
-draw_star(t, 200, 200, 100, 120, 'right', 'red') # draw a five-pointed star
+t = create_turtle("red", "yellow")  # create a turtle object
+for x in range(-200, 0, 25):  # loop four times
+    draw_square(t, x, x, 100, "left", "#F5DEB3")  # draw a square
 
-
+draw_star(t, 200, 200, 100, 120, "right", "red")  # draw a five-pointed star
--- virus_transmission.py
+++ virus_transmission.py
@@ -6,14 +6,14 @@ Do not run this file directly.
 Rather, call this function from main.py and run that file.
 """
 
-def calculate_infections(starting_number_infections, reproduction_rate, num_days):
-  """
-  Write a function that predicts the approximate number of infected individuals from a socially-transmitted viral infection after a given number of days.
-  Assume that, once infected, individuals stay infected indefinitely.
-  You must use a for loop to progressively calculate the number of infections each day until the target number of days is reached.
 
-  :param starting_number_infections: The number of infected individuals to begin with.
-  :param reproduction_rate: A floating point number indicating the rate of growth in the number of infections each day.
-  :returns: The new number of infected individuals after the given number of days, rounded to the nearest integer.
-  """
-  
+def calculate_infections(starting_number_infections, reproduction_rate, num_days):
+    """
+    Write a function that predicts the approximate number of infected individuals from a socially-transmitted viral infection after a given number of days.
+    Assume that, once infected, individuals stay infected indefinitely.
+    You must use a for loop to progressively calculate the number of infections each day until the target number of days is reached.
+
+    :param starting_number_infections: The number of infected individuals to begin with.
+    :param reproduction_rate: A floating point number indicating the rate of growth in the number of infections each day.
+    :returns: The new number of infected individuals after the given number of days, rounded to the nearest integer.
+    """
